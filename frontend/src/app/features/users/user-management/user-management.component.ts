9020490c01056bd4d489a14295300d5dadd41aa2
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2026-04-10 00:11:06 -0600

    Add load Users function to component

---
+++ frontend/src/app/features/users/user-management/user-management.component.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+import { SessionService } from '../../../core/services/session.service';
+import { UsersService } from '../../../core/services/users.service';
+import { RestaurantMembership, User } from '../../../core/models/user.model';
+
+@Component({
+  selector: 'app-user-management',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
+  ],
+  templateUrl: './user-management.component.html',
+  styleUrl: './user-management.component.css',
+})
+export class UserManagementComponent implements OnInit {
+  private sessionService = inject(SessionService);
+  private userService = inject(UsersService);
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
+
+  restaurantId = '';
+  restaurantName = '';
+
+  users: User[] = [];
+  members: RestaurantMembership[] = [];
+
+  loadingUsers = false;
+  loadingMembers = false;
+  error = '';
+  successMessage = '';
+
+  roleOptions: Array<'OWNER' | 'MANAGER' | 'CASHIER'> = ['OWNER', 'MANAGER', 'CASHIER'];
+
+  createUserForm = this.fb.group({
+    fullName: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]]
+  });
+
+  addMemberForm = this.fb.group({
+    userId: ['', [Validators.required]],
+    role: ['CASHIER' as 'OWNER' | 'MANAGER' | 'CASHIER', [Validators.required]]
+  });
+
+  ngOnInit(): void {
+    if (!this.sessionService.isOwner()) {
+      this.router.navigate(['/pos']);
+      return;
+    }
+
+    this.restaurantId = this.sessionService.getCurrentRestaurantId();
+    this.restaurantName = this.sessionService.getCurrentRestaurantName();
+
+    if (!this.restaurantId) {
+      this.router.navigate(['/login']);
+      return;
+    }
+
+    this.loadUsers
+
+  }
+
+  loadUsers(): void {
+    this.loadingUsers = true;
+    this.userService.getUsers().subscribe({
+      next: (data) => {
+        this.users = data;
+        this.loadingUsers = false;
+      },
+      error: () => {
+        this.error = 'No se pudieron cargar los usuarios';
+        this.loadingUsers = false;
+      }
+    });
+  }
+}
