96abd3b59b257939f2f49904f74c7fa226bc33de
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2026-04-09 23:34:17 -0600

    Add User Servie

---
+++ frontend/src/app/core/services/users.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import {
+  AddUserToRestaurantRequest,
+  CreateUserRequest,
+  RestaurantMembership,
+  User
+} from '../models/user.model'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  private http = inject(HttpClient);
+  private baseUrl = `${environment.apiUrl}`;
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.baseUrl}/users`);
+  }
+
+  createUser(payload: CreateUserRequest): Observable<User> {
+    return this.http.post<User>(`${this.baseUrl}/users`, payload);
+  }
+
+  getRestaurantMembers(restaurantId: string): Observable<RestaurantMembership[]> {
+    return this.http.get<RestaurantMembership[]>(`${this.baseUrl}/restaurants/${restaurantId}/members`);
+  }
+
+  addUserToRestaurant(
+    restaurantId: string,
+    payload: AddUserToRestaurantRequest
+  ): Observable<RestaurantMembership> {
+    return this.http.post<RestaurantMembership>(
+      `${this.baseUrl}/restaurants/${restaurantId}/members`,
+      payload
+    );
+  }
+}
